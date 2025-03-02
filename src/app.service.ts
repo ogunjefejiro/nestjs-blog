0b0a4ec46fdd8ca8bdd8f3615aa194745226e48f
Fejiro Ogunje <ogunjefejiro@gmail.com>
2025-03-02 15:05:23 +0100

    chore: made base app endpoint public

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common"
 @Injectable()
 export class AppService {
    getHello(): string {
-      return "Hello Feji My Guy!"
+      return "Welcome to my Blog!. I built this with NestJs"
    }
 }
